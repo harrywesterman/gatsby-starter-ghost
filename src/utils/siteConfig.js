703ef07b6b6b252b4b37f39f9a318d4fe65937e1
harrywesterman <harrywesterman@gmail.com>
2023-01-02 11:25:37 +0100

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,15 +1,11 @@
 const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl: `https://thuisbrouwer.westermanonline.com`,
-
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
-
+    postsPerPage: 12,
     siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
-
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
-
     shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
